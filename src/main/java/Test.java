d4ff509d494fe361cf5315bf47deb6278ad37367
kuuhaku <2562776875@qq.com>
2022-11-17 12:06:19 +0800

    new 4

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,12 @@
 public class Test {
     public static void main(String[] args) {
+        System.out.println(66666);
         System.out.println();
         System.out.println();
         System.out.println();
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println();
+        System.out.println(66666666);
     }
 }
